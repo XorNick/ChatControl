697236a4a59d317f9c36200bf84bc16bf4a707a9
XorNick <redducknick@gmail.com>
2017-09-13 15:58:04 -0400

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# Chat Control
+Simple plugin request I found (on Spigot) and decided why the hell not do it and opensource it.
---
+++ src/main/java/net/xornick/chatcontrol/ChatControl.java
@@ -0,0 +1,12 @@
+package net.xornick.chatcontrol;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class ChatControl extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+}
