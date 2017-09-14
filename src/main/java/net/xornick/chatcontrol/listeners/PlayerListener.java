12dff694ca5e73da8a66655624a1f40cae37fe0b
XorNick <redducknick@gmail.com>
2017-09-14 05:15:55 -0400

    let's remove them from the list if they, y'know, moved

--- src/main/java/net/xornick/chatcontrol/listeners/PlayerListener.java
+++ src/main/java/net/xornick/chatcontrol/listeners/PlayerListener.java
@@ -4,6 +4,7 @@ import net.xornick.chatcontrol.ChatControl;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerJoinEvent;
+import org.bukkit.event.player.PlayerMoveEvent;
 
 public class PlayerListener implements Listener {
 
@@ -14,5 +15,16 @@ public class PlayerListener implements Listener {
         if (plugin.blockInteractionUntilPlayerMove) {
             plugin.moved.add(event.getPlayer().getUniqueId());
         }
+    }
+
+    @EventHandler
+    public void onPlayerMove(PlayerMoveEvent event) {
+        if (!plugin.blockInteractionUntilPlayerMove) {
+            return;
+        }
+        if (event.getFrom().getBlockX() == event.getTo().getBlockX() && event.getFrom().getBlockZ() == event.getTo().getBlockZ()) {
+            return;
+        }
+        plugin.moved.remove(event.getPlayer().getUniqueId());
     }
 }
